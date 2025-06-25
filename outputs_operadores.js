da18a14e34ec4ba29eafa40dc791b704df8ce6f9
Peetii <merxspace@outlook.com>
2025-06-25 12:51:27 -0300

    outputs e operadores

--- outputs_operadores.js
+++ outputs_operadores.js
@@ -61,3 +61,67 @@ let num_sub = 15;
 let num2_sub = 7;
 const result_sub = num_sub - num2_sub;
 console.log("Subtração: ",result_sub,"Atribuição subtração: ", num_sub -= num2_sub); //operador de atribuição p/ soma
+
+//Operadores de comparação
+//Adicionando img da tabela dos operadores de comparação
+
+//titulo
+const titulo_comp = document.createElement("h2");
+titulo_comp.innerText = "Tabela de Operadores de comparação (Ref W3Schools): ";
+document.getElementById("img-tabela-container").append(titulo_comp);
+
+//criar tag img
+const comp_img_table = document.createElement("img");
+comp_img_table.src = "assets/comp_operadores.png";
+comp_img_table.alt = "Tabela de Operadores de comparação JS";
+comp_img_table.width = 700;
+comp_img_table.height = 360;
+
+document.getElementById("img-tabela-container").appendChild(comp_img_table);
+
+
+//Igual à
+let x = 5;
+console.log("is x equals 8? ", x == 8); // == verifica valor igual/ return false
+
+console.log("is x equals 5? ", x == "5"); // == verifica valor igual(mesmo que o tipo seja string,em js por padrao  verifica o valor de dentro da string, retornando true)/ return  true
+
+console.log("is x equals 5? ", x === "5"); // === verifica valor e o tipo/ return false
+
+console.log("is x equals 5? ", x != "5"); // verifica valor diferente/ return true
+
+console.log("is x equals 5? ", x !== "5"); // verifica se o valor e o tipo sao diferente/ return true
+
+console.log("is x equals 5? ", x >= "5"); // verifica valor maior ou igual a 5/ return true
+
+console.log("is x equals 5? ", x < "5"); // verifica se o valor menor que 5/ return false
+
+
+
+//Operadores Logicos
+//Adicionando img da tabela dos operadores Logicos
+
+//titulo
+const titulo_op_logico = document.createElement("h2");
+titulo_op_logico.innerText = "Tabela de Operadores Logicos (Ref W3Schools): ";
+document.getElementById("img-tabela-container").append(titulo_op_logico);
+
+//criar tag img
+const logico_img_table = document.createElement("img");
+logico_img_table.src = "assets/op_logico.png";
+logico_img_table.alt = "Tabela de Operadores Logicos JS";
+logico_img_table.width = 700;
+logico_img_table.height = 250;
+
+document.getElementById("img-tabela-container").appendChild(logico_img_table);
+
+
+let x1 = 6;
+let y1 = 3;
+
+console.log(x1 < 10 && y1 > 1);// true
+
+console.log(x1 == 5 || y1 ==  5);// falso
+
+console.log("6 e 3 não são iguais? ",!(x1 == y1));// true
+
