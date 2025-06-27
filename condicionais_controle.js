e89ac4b655a7b6e671b3e677e6f2e95f992282d0
Peetii <merxspace@outlook.com>
2025-06-26 23:14:30 -0300

    condicionais e de  controle

---
+++ condicionais_controle.js
@@ -0,0 +1,29 @@
+//Condicionais
+
+if(true) {
+    let yes = "YES!";
+
+    if (yes === "YES!"){
+        console.log("SIIIIM !!!")
+    }
+}
+/*
+let idade = 4;
+
+if(idade <= 10 && idade > 5) {
+    console.log(`${idade} anos, você é criança.`);
+}
+else if(idade <= 5 ){
+    console.log(`${idade} anos, você é pré.`);
+}
+else if(idade > 10 && idade < 18){
+    console.log(`${idade} anos, você é jovem.`);
+}
+else if(idade > 18 && idade < 60){
+    console.log(`${idade} anos, você é adulto.`);
+}
+else {
+    console.log(`${idade} anos, você é idoso.`);
+}
+
+*/
